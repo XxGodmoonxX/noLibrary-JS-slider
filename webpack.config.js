c9c7a5f98160f228da3c1e60c622984c3f966e60
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2020-02-17 01:10:23 +0900

    webpackの諸々試すために一旦Qiitaに沿ってやってみる

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+// 参考リンク
+// https://qiita.com/soarflat/items/28bf799f7e0335b68186
+
+// output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
+const path = require("path");
+
+module.exports = {
+  // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
+  mode: "development",
+  // エントリーポイント 各モジュールを読み込んで処理するJS
+  entry: './src/app.js',
+  // 出力の設定
+  output: {
+    // 出力ファイル名
+    filename: 'app.js',
+    // 出力するパス名
+    path: path.join(__dirname, 'public/js')
+  }
+};
