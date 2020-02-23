6c10c3e77c8b4d52b37e7534f2d2f31fe0449052
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2020-02-23 23:35:58 +0900

    success to clone img node

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,14 @@
-import { xxxxx } from './utils';
+window.addEventListener('DOMContentLoaded', () => {});
+
+window.addEventListener('load', () => {
+  const slider = document.querySelector('.slider');
+  const sliderWrapper = document.querySelector('.slider__wrapper');
+  const sliderItem = document.querySelectorAll('.slider__item');
+  const sliderItemArray = Array.prototype.slice.call(sliderItem);
+
+  const firstSliderItemClone = sliderItemArray[0].cloneNode(true);
+  const lastSliderItemClone = sliderItemArray[sliderItemArray.length - 1].cloneNode(true);
+
+  sliderWrapper.append(firstSliderItemClone);
+  sliderWrapper.prepend(lastSliderItemClone);
+});
